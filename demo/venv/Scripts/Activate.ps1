3a04978b7c7a300631445b78dc7e7c68cd884186
mingyu.ding <15311449479@163.com>
2019-11-28 17:04:17 +0800

    demo

--- demo/venv/Scripts/Activate.ps1
+++ demo/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Users\Flask\01_demo\venv"
+$env:VIRTUAL_ENV="D:\Users\Flask\demo\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
